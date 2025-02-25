605f2c8a72bc9c88ae64424eb8ad9e78f909eeda
AlexanderVeraYate <u20242227721@usco.edu.co>
2025-02-25 10:26:08 -0500

    Update Segundo.java

--- Proyect_Alex/Taller/src/Segundo.java
+++ Proyect_Alex/Taller/src/Segundo.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Segundo {
@@ -9,28 +8,23 @@ public class Segundo {
         System.out.println("_____Cuantos llantas va a comprar______");
         int num1 = entrada.nextInt();
         int total_U;
-
-
-        if (num1 <= 5) {
+        if (num1 < 5) {
             int valor = 100;
             total_U = valor * num1;
-            System.out.println("Vas a comprar " + num1 + " y el precio de cada unidad es: " + "U$" + valor + " y el total de la compra es: " + total_U);
-        } else if (num1 <= 10) {
+            System.out.println("Vas a comprar " + num1 + " y el precio de cada unidad es: " + "U$" + valor + " y el total de la compra es: U$" + total_U);
+        } else {
+            if (num1 < 10) {
                 int valor = 75;
                 total_U = valor * num1;
-                System.out.println("Vas a comprar " + num1 + " y el precio de cada unidad es: " + "U$" + valor + " y el total de la compra es: " + total_U);
+                System.out.println("Vas a comprar " + num1 + " y el precio de cada unidad es: " + "U$" + valor + " y el total de la compra es: U$" + total_U);
             } else {
                 int valor = 50;
                 total_U = valor * num1;
-                System.out.println("Vas a comprar " + num1 + " y el precio de cada unidad es: " + "U$" + valor + " y el total de la compra es: " + total_U);
-
+                System.out.println("Vas a comprar " + num1 + " y el precio de cada unidad es: " + "U$" + valor + " y el total de la compra es: U$" + total_U);
             }
-
         }
-
     }
-
-
+}
 // En una serviteca se ha establecido una promoción de las llantas marca “Pinchadas”, que consiste
 // en lo siguiente:
 // • Si se compran menos de cinco llantas el precio es de U$100 cada una, de U$75 si se
