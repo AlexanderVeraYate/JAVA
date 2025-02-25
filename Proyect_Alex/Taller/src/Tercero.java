0f2c40c6b81df2cdca233aeda8b909ae29442763
AlexanderVeraYate <u20242227721@usco.edu.co>
2025-02-25 10:26:28 -0500

    Update Tercero.java

--- Proyect_Alex/Taller/src/Tercero.java
+++ Proyect_Alex/Taller/src/Tercero.java
@@ -11,43 +11,38 @@ public class Tercero {
         System.out.println("____es marca ´NOSY´?____ (Seleccionar 1.si/2.no)");
         int num1= Entrada.nextInt();
         
-
         switch (num1) {
             case 1:
             if (Compra>=500) {
                 Total =Compra - Compra * ((float)Descuento + (float)Descuento_Extra);
                 Total = Total + (Total * (float)iva);
-                System.out.println("La compra total fue de: "+Total);
+                System.out.println("La compra total fue de: U$"+Total);
                 
             } else {
                 Total =Compra - Compra * (float)Descuento_Extra;
                 Total = Total + (Total * (float)iva);
-                System.out.println("La compra total fue de: "+Total);
-                
-            }
-                break;
+                System.out.println("La compra total fue de: U$"+Total);
+
+            } break;
                 
                 case 2:
                 if (Compra>=500) {
                     Total =Compra - Compra * (float)Descuento;
                     Total = Total + (Total * (float)iva);
-                    System.out.println("La compra total fue de: "+Total);
+                    System.out.println("La compra total fue de: U$"+Total);
                     
                 } else {
                     Total = Compra + (Compra * (float)iva);
-                    System.out.println("La compra total fue de: "+Total);
+                    System.out.println("La compra total fue de: U$"+Total);
                     
-                }
-                break;
+                } break;
         
             default:
             System.err.println("Error 404: Entrada no admitida");
                 break;
-
         }
     }
 }
-
 // Un proveedor de reproductores de música ofrece un descuento del 10% sobre el precio sin IVA,
 // de algún aparato, si esta cuesta U$500 o más. Además, independientemente de esto, ofrece un
 // 5% de descuento si la marca es “NOSY”. Determinar cuánto pagara, con IVA incluido, un cliente
